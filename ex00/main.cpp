5efbb3502e63caf4f86b0151b685e23223a51862
Winspear22 <adnen.aloui2@hotmail.com>
2023-05-27 11:11:49 +0200

    CPP09 -- ex00 ok for file that are not text

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,12 +1,28 @@
 # include "BitcoinExchange.hpp"
 
-bool IsExecutable(const std::string& filename) 
+bool IsTextFile(const std::string& filename) 
 {
+    std::ifstream file(filename.c_str(), std::ios::binary);
+    
+    if (!file)
+        return false;
+    
+    char c;
+    size_t count = 0;
+
+    while (file.get(c) && count++ < 512) 
+    {
+        if (!std::isprint(c) && !std::isspace(c))
+            return false;
+    }
+
     if (access(filename.c_str(), X_OK) == 0) 
         return (SUCCESS);
 	else 
         return (FAILURE);
+    return true;
 }
+
 
 int main(int argc, char **argv) 
 {
@@ -22,8 +38,18 @@ int main(int argc, char **argv)
     std::string line;
     std::ifstream database("data.csv");
     BitcoinExchange btc;
-    if(database.is_open()) 
+    if (!IsTextFile(argv[1])) 
+    {
+        std::cerr << RED << "Error: " << WHITE << argv[1] << RED <<" is not a text file." << NORMAL << std::endl;
+        return (FAILURE);
+    }
+    if (database.is_open()) 
 	{
+        if (database.peek() == EOF)
+        {
+            std::cout << RED << "Error:" << WHITE " data.csv " << RED << "is empty." << NORMAL << std::endl;
+            return (FAILURE); 
+        }
         while(getline(database, line)) 
             btc.SplitByCharacter(line);
         database.close();
@@ -34,15 +60,14 @@ int main(int argc, char **argv)
         return (FAILURE);
     }
 
-    if (!IsExecutable(argv[1])) 
-	{
-        std::cerr << RED << "Error: file is not a text file." << NORMAL << std::endl;
-        return (FAILURE);
-    }
-
     std::ifstream file(argv[1]);
     if (file.is_open()) 
 	{
+        if (file.peek() == EOF)
+        {
+            std::cout << RED << "Error " << WHITE << argv[1] << RED << " is empty." << NORMAL << std::endl;
+            return (FAILURE); 
+        }
         while(getline(file, line)) 
 		{
             if(btc.CheckForErrorsInInput(line) == SUCCESS)
